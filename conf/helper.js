5d6e1788645b481e267211548b1b69f8ddc62997
VuongwNguyen <nguyenvuongw134@gmail.com>
2024-04-04 02:07:04 +0700

    CategoryWithProducts

--- conf/helper.js
+++ conf/helper.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-// const local = 'mongodb+srv://doadmin:B974N0e58CI1EP2z@db-mongodb-sgp1-69243-6223c8f3.mongo.ondigitalocean.com/admin?authSource=admin&tls=true';
-const local ='mongodb://localhost:27017/dbOfVuongw'
+const local = 'mongodb+srv://doadmin:B974N0e58CI1EP2z@db-mongodb-sgp1-69243-6223c8f3.mongo.ondigitalocean.com/admin?authSource=admin&tls=true';
+// const local ='mongodb://localhost:27017/dbOfVuongw'
 async function connect() {
     try {
         await mongoose.connect(local, {
