d514917b7bcb2094c520907dd5d7b038ff8ac020
VuongwNguyen <nguyenvuongw134@gmail.com>
2024-04-04 11:33:11 +0700

    @reapair cart for user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -21,7 +21,7 @@ async function register(username, phone, email, password) {
 
 async function login(username, password) {
     try {
-        const user = await Users.findOne({ $or: [{ phone: username }, { email: username }] }).populate('cart.product_id','cart.product_id.category_id');
+        const user = await Users.findOne({ $or: [{ phone: username }, { email: username }] }).populate('cart.product_id');
         if (user) {
             if (user.status == 'deleted') {
                 return null;
